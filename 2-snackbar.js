e8df4c51f3161238ebbcd6fa66823a66c15cb6a7
StasSkv <stas000123@gmail.com>
2025-02-06 21:55:54 +0000

    Deploying to gh-pages from @ StasSkv/goit-js-hw-10@66d789c206b7b8c81d7291a4a1339c53a6a52550 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,9 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector("input"),e=s.closest("label");s.classList.add("input-delay");e.classList.add("label-delay");s.insertAdjacentHTML("afterend",`<div class="container">
+  <svg class="svg" width="16" height="16">
+    <use href="./img/plus.svg#plus"></use>
+  </svg>
+  <svg class="svg" width="16" height="16">
+    <use href="./img/minus#minus.svg"></use>
+  </svg>
+</div>`);
+//# sourceMappingURL=2-snackbar.js.map
